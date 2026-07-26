163a3e60f532e738cf13c0c3cdecec80400ad034
CherryIT <turndargon@hotmail.com>
2026-07-26 17:17:52 +0800

    Auto update blog

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+cd /d "D:\Desktop\quartz"
+
+echo [1/3] Adding changes...
+git add .
+
+echo [2/3] Committing changes...
+git commit -m "Auto update blog"
+
+echo [3/3] Pushing to GitHub...
+git push origin main
+
+echo.
+echo ====================================
+echo  Success! Cloudflare is building.
+echo ====================================
+timeout /t 3
